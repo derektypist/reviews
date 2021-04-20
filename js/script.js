4a585fe5bb78d1a83cc9c253155b899ac4815531
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-20 15:51:10 +0000

    Updated script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Set Up Reviews
+
+const reviews = [
+    {
+        id: 1,
+        name: "Irina Nita",
+        job: "Junior Software Developer",
+        img: "images/irina-nita.PNG",
+        text: "Well done"
+    }, 
+
+    {
+        id: 2,
+        name: "Stuart Crang",
+        job: "IT Career Consultant",
+        img: "images/stuart-crang.PNG",
+        txt: "Well done"
+    },
+
+    {
+        id: 3,
+        name: "Cindy Marchena",
+        job: "Junior Developer",
+        img: "images/cindy-marchena.PNG",
+        txt: "Congratulations"
+
+    }
+]
