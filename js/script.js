079e5c658b495bbe5f235cd3a6167eff10186468
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-20 16:07:13 +0000

    Added currentItem at script.js

--- js/script.js
+++ js/script.js
@@ -41,3 +41,9 @@ const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// Set Starting Item
+let currentItem = 0;
